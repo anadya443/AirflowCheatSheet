07da07e87cdd1a192348c84a66a18c42ffa3962b
anadya443 <137520608+anadya443@users.noreply.github.com>
2023-07-28 13:03:16 +0200

    Update child_dag.py

--- child_dag.py
+++ child_dag.py
@@ -1,4 +1,5 @@
 import pendulum
+import pickle
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
@@ -15,22 +16,29 @@ external_task_id="parent_dag_task"
 def get_most_recent_run_today(dag_id):
     today_date = datetime.now(timezone.utc)
     dag_runs = list(DagRun.find(dag_id=dag_id))
-    dag_runs.sort(key=lambda x: x.execution_date, reverse=True)
+    dag_runs.sort(key=lambda x: x.start_date, reverse=True)
     if dag_runs:
-        recent_run = dag_runs[0].execution_date;
-        if (recent_run.date() == today_date.date()):
-            return recent_run;
+        recent_run_start_date = dag_runs[0].start_date
+        recent_run_execution_date = dag_runs[0].execution_date
+        if (recent_run_start_date.date() == today_date.date()):
+            return recent_run_execution_date;
         else: 
             return today_date;
     else:
         return today_date;
 
+
+def get_most_recent_dag():
+    today_date = datetime.now(timezone.utc)
+    dag_runs = list(DagRun.find(dag_id=external_dag_id))
+    dag_runs.sort(key=lambda x: x.execution_date, reverse=True)
+    return dag_runs[0]
+
 def get_most_recent_external_dag_today(dt):
     return get_most_recent_run_today(external_dag_id);
 
 last_run_today = get_most_recent_external_dag_today(None)
-
-
+recent_dag = get_most_recent_dag()
 
 with DAG(
     dag_id=dag_id,
@@ -52,7 +60,7 @@ with DAG(
     child_task2 = BashOperator(
         task_id="print_date",
         depends_on_past=False,
-        bash_command=f"echo hello { last_run_today}",
+        bash_command=f"echo hello { recent_dag.start_date} {recent_dag.execution_date}",
         retries=3,
     )
     child_task2 >> child_task1
